74054236caa4e268354a8897f80fcb356bffab87
Qudret Abdullayev <abdullayevqudret97@gmail.com>
2025-09-02 09:49:43 +0400

    test 2

--- src/app/VideoStatic/VideoStatic.js
+++ src/app/VideoStatic/VideoStatic.js
@@ -4,10 +4,9 @@ import styles from './VideoStatic.module.scss';
 
 const VideoStatic = ({ src, props }) => {
     return (
+        <div className={styles.video}>
         <video
             {...props}
-
-            className={styles.video}
             autoPlay={true} loop={true} muted={true} playsInline={true} 
             type="video/mp4"
             poster="https://video.y.co/upload/f_auto,q_auto,c_fill,so_auto,ar_16:9,w_2560/v1708037304/landing-video-homepage-2022_nqvybq.jpg"
@@ -15,6 +14,7 @@ const VideoStatic = ({ src, props }) => {
             <source src={src} type="video/mp4" />
             Your browser does not support the video tag.
         </video>
+        </div>
     );
 };
 
